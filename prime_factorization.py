f3ddcb9778a7985fa4ae6507a26185826df23c0d
Vinay Shivaji Nikam <vinayn.gceme@gmail.com>
2019-08-28 20:16:11 +0530

    Update prime_factorization.py

--- prime_factorization.py
+++ prime_factorization.py
@@ -1,4 +1,4 @@
-def prime_numbers(n):
+def prime_numbers(n): # Function to find out prime numbers
     primes = [2]
     for x in range(3,n+1,2):
         for y in range(3,n+1):
@@ -11,39 +11,43 @@ def prime_numbers(n):
                     break
     return primes
 
-while True:
-    factors = []
-    given_num = input("\nEnter a number to factorize or enter 'quit' to exit: ")
-    if given_num.isdigit():
-        num = int(given_num)
-        prime_nums = prime_numbers(num)
+def main():  # Wrapping function
+    while True:
+        factors = []
+        given_num = input("\nEnter a number to factorize or enter 'quit' to exit: ")
+        if given_num.isdigit():
+            num = int(given_num)
+            prime_nums = prime_numbers(num)
 
-        if num in prime_nums:
-            print('Given number is prime number !')
-            print(f'Factors of given numbers are: 1*{num}')
-            continue
-        elif num <= 0:
-            print('Wrong Input!')
+            if num in prime_nums:
+                print('Given number is prime number !')
+                print(f'Factors of given numbers are: 1*{num}')
+                continue
+            elif num <= 0:
+                print('Wrong Input!')
+                continue
+            else:
+                i = 0    
+                while prime_nums[i] <= num:
+                    remainder = num%prime_nums[i]
+                    if remainder == 0:
+                        factors.append(prime_nums[i])
+                        num = num/prime_nums[i]
+                        continue
+                    else:
+                        i += 1
+                        continue
+                    break
+
+            print(f'Factors of given numbers are: ',end = '')
+            print('*'.join(str(number) for number in factors))
             continue
         else:
-            i = 0    
-            while prime_nums[i] <= num:
-                remainder = num%prime_nums[i]
-                if remainder == 0:
-                    factors.append(prime_nums[i])
-                    num = num/prime_nums[i]
-                    continue
-                else:
-                    i += 1
-                    continue
+            if given_num == 'quit':
                 break
-
-        print(f'Factors of given numbers are: ',end = '')
-        print('*'.join(str(number) for number in factors))
-        continue
-    else:
-        if given_num == 'quit':
-            break
-        else:
-            print('Wrong input!')
-            continue
+            else:
+                print('Wrong input!')
+                continue
+        
+if __name__ == "__main__":
+    main()
